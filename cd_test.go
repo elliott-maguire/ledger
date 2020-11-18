b3408a338a641205ee6cf265caf0c0b54f63a113
Elliott Maguire <me@elliott-m.com>
2020-11-17 16:13:28 -0800

    add a unit test for Compare

---
+++ cd_test.go
@@ -0,0 +1,50 @@
+package brickhouse
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCompare(t *testing.T) {
+	d1 := map[string]interface{}{
+		"1": map[string]interface{}{
+			"a": "foo",
+			"b": "bar",
+			"c": "baz",
+		},
+		"2": map[string]interface{}{
+			"a": "foo",
+			"b": "bar",
+			"c": "baz",
+		},
+		"3": map[string]interface{}{
+			"a": "foo",
+			"b": "bar",
+			"c": "baz",
+		},
+	}
+	d2 := map[string]interface{}{
+		"1": map[string]interface{}{
+			"a": "zoo",
+			"b": "bar",
+			"c": "baz",
+		},
+		"3": map[string]interface{}{
+			"b": "bar",
+			"c": "baz",
+			"d": "zoo",
+		},
+		"4": map[string]interface{}{
+			"a": "foo",
+			"b": "bar",
+			"c": "baz",
+		},
+	}
+
+	changes := Compare(d1, d2)
+	if len(changes) != 5 {
+		t.Error("failed")
+	} else {
+		fmt.Println(changes)
+	}
+}
