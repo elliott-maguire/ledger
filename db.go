94b9a357b7371432c7a16a756d051d23eabaf001
Elliott Maguire <me@elliott-m.com>
2020-11-17 22:51:51 -0800

    restructure to support the writing of Change maps

--- db.go
+++ db.go
@@ -6,10 +6,20 @@ import (
 	"strings"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
 )
 
 var reField = regexp.MustCompile("[^0-9A-Za-z_]")
 var reValue = regexp.MustCompile("['\r\n\t]")
+
+func createFieldset(fields []string) string {
+	cleaned := make([]string, len(fields))
+	for i, f := range fields {
+		cleaned[i] = fmt.Sprintf("%s VARCHAR", reField.ReplaceAllString(f, ""))
+	}
+
+	return strings.Join(cleaned, ",")
+}
 
 // Table is a pseudo-enumerator for indicating which table to write/read to/from.
 type Table string
@@ -22,47 +32,22 @@ const (
 )
 
 // Ensure the given store exists on the database and is accessible.
-func Ensure(db *sqlx.DB, label string, fields ...string) error {
-	var q string
-
+func Ensure(db *sqlx.DB, label string) error {
 	tx, err := db.Begin()
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
 
-	q = fmt.Sprintf("CREATE SCHEMA IF NOT EXISTS %s", label)
-	if _, err := tx.Exec(q); err != nil {
+	createSchema := fmt.Sprintf("CREATE SCHEMA IF NOT EXISTS %s", label)
+	if _, err := tx.Exec(createSchema); err != nil {
 		return err
 	}
 
-	if len(fields) > 0 {
-		base := "CREATE TABLE IF NOT EXISTS %s.%s (%s)"
-
-		createFieldset := func(fields []string) string {
-			cleaned := make([]string, len(fields))
-			for i, f := range fields {
-				cleaned[i] = fmt.Sprintf("%s VARCHAR", reField.ReplaceAllString(f, ""))
-			}
-
-			return strings.Join(cleaned, ",")
-		}
-
-		fieldset := createFieldset(append(fields, "brickhouse_id"))
-		q = fmt.Sprintf(base, label, Live, fieldset)
-		if _, err := tx.Exec(q); err != nil {
-			return err
-		}
-		q = fmt.Sprintf(base, label, Archive, fieldset)
-		if _, err := tx.Exec(q); err != nil {
-			return err
-		}
-
-		changeFields := []string{"id", "timestamp", "operation", "old", "new"}
-		fieldset = createFieldset(changeFields)
-		if _, err := tx.Exec(fmt.Sprintf(base, label, Changes, fieldset)); err != nil {
-			return err
-		}
+	createChangesTable := "CREATE TABLE IF NOT EXISTS %s.%s (%s)"
+	fieldset := createFieldset([]string{"brickhouse_id", "timestamp", "operation", "old", "new"})
+	if _, err := tx.Exec(fmt.Sprintf(createChangesTable, label, Changes, fieldset)); err != nil {
+		return err
 	}
 
 	if err := tx.Commit(); err != nil {
@@ -74,15 +59,18 @@ func Ensure(db *sqlx.DB, label string, fields ...string) error {
 
 // Read from the indicated table in the labeled store.
 func Read(db *sqlx.DB, label string, table Table) (*map[string]interface{}, error) {
-	var q string
-
 	if err := Ensure(db, label); err != nil {
 		return nil, err
 	}
 
-	q = fmt.Sprintf("SELECT * FROM %s.%s", label, table)
-	rows, err := db.Queryx(q)
+	selectAll := fmt.Sprintf("SELECT * FROM %s.%s", label, table)
+	rows, err := db.Queryx(selectAll)
 	if err != nil {
+		if _, is := err.(*pq.Error); is && err.(*pq.Error).Code == "42P01" {
+			out := make(map[string]interface{})
+			return &out, nil
+		}
+
 		return nil, err
 	}
 
@@ -103,12 +91,9 @@ func Read(db *sqlx.DB, label string, table Table) (*map[string]interface{}, erro
 }
 
 // Write to the indicated table in the labeled store.
-func Write(db *sqlx.DB, label string, table Table, data map[string]interface{}, args ...bool) error {
-	var q string
-
-	drop := false
-	if len(args) > 0 {
-		drop = args[0]
+func Write(db *sqlx.DB, label string, table Table, data *map[string]interface{}, args ...bool) error {
+	if err := Ensure(db, label); err != nil {
+		return err
 	}
 
 	tx, err := db.Begin()
@@ -117,39 +102,50 @@ func Write(db *sqlx.DB, label string, table Table, data map[string]interface{}, 
 	}
 	defer tx.Rollback()
 
-	if drop {
-		q = fmt.Sprintf("DROP TABLE IF EXISTS %s.%s", label, table)
-		if _, err := tx.Exec(q); err != nil {
-			return err
-		}
-	} else {
-		q = fmt.Sprintf("TRUNCATE TABLE IF EXISTS %s.%s", label, table)
-		if _, err := tx.Exec(q); err != nil {
-			return err
-		}
-	}
-
-	ensured := false
-	for id, record := range data {
-		fields := make([]string, 0)
-		values := make([]string, 0)
+	var fields []string
+	var values []string
+	insertions := make([]string, 0)
+	for id, record := range *data {
+		fields = make([]string, 0)
+		values = make([]string, 0)
 
 		for k, v := range record.(map[string]interface{}) {
 			fields = append(fields, k)
 			values = append(values, fmt.Sprintf("'%s'", reValue.ReplaceAllString(v.(string), "")))
 		}
 
-		if !ensured {
-			if err := Ensure(db, label, fields...); err != nil {
-				return err
-			}
-		}
-
 		fieldset := strings.Join(fields, ",")
 		valueset := strings.Join(values, ",")
 
-		q = fmt.Sprintf("INSERT INTO %s.%s (brickhouse_id,%s) VALUES ('%s',%s)", label, table, fieldset, id, valueset)
-		if _, err := tx.Exec(q); err != nil {
+		insertion := fmt.Sprintf(
+			"INSERT INTO %s.%s (brickhouse_id,%s) VALUES ('%s',%s)",
+			label, table, fieldset, id, valueset,
+		)
+		insertions = append(insertions, insertion)
+	}
+
+	fieldset := createFieldset(append(fields, "brickhouse_id"))
+	createTable := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s.%s (%s)", label, table, fieldset)
+	if _, err := tx.Exec(createTable); err != nil {
+		return err
+	}
+
+	drop := true
+	if len(args) > 0 {
+		drop = args[0]
+	}
+	if drop {
+		dropTable := fmt.Sprintf("DROP TABLE %s.%s", label, table)
+		if _, err := tx.Exec(dropTable); err != nil {
+			return err
+		}
+		if _, err := tx.Exec(createTable); err != nil {
+			return err
+		}
+	}
+
+	for _, insertion := range insertions {
+		if _, err := tx.Exec(insertion); err != nil {
 			return err
 		}
 	}
