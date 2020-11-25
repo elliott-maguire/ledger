fb77996cfd92f994ade82b9b5ddfb10d87f2a981
Elliott Maguire <me@elliott-m.com>
2020-11-24 16:19:58 -0800

    polish up Restore and add test

--- io.go
+++ io.go
@@ -1,6 +1,11 @@
 package brickhouse
 
 import (
+	"encoding/json"
+	"errors"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -13,11 +18,11 @@ func Update(db *sqlx.DB, label string, data *map[string]interface{}) error {
 		return err
 	}
 
-	changes := Compare(*old, *data)
+	changes := Compare(old, data)
 	mappedChanges := make(map[string]interface{})
 	for _, change := range changes {
-		id, record := change.Map()
-		mappedChanges[id] = record
+		id, mapped := change.Map()
+		mappedChanges[id] = *mapped
 	}
 	if err := Write(db, label, Changes, &mappedChanges, false); err != nil {
 		return err
@@ -31,6 +36,83 @@ func Update(db *sqlx.DB, label string, data *map[string]interface{}) error {
 }
 
 // Restore the `archive` table to a given time.
-func Restore(db *sqlx.DB, label string, time time.Time) (*[][]string, error) {
-	return nil, nil
+func Restore(db *sqlx.DB, label string, target time.Time) (*map[string]interface{}, error) {
+	data, err := Read(db, label, Live)
+	if err != nil {
+		return nil, err
+	}
+
+	rawChanges, err := Read(db, label, Changes)
+	if err != nil {
+		return nil, err
+	}
+	var changes []Change
+	for k, v := range *rawChanges {
+		record := v.(map[string]interface{})
+
+		keychain := record["keychain"].(string)
+		timestamp, err := time.Parse(time.RFC3339Nano, record["timestamp"].(string))
+		if err != nil {
+			return nil, err
+		}
+		operation, err := strconv.Atoi(record["operation"].(string))
+		if err != nil {
+			return nil, err
+		}
+
+		old := record["old"].(string)
+		new := record["new"].(string)
+
+		change := Change{
+			ID:        k,
+			Keychain:  keychain,
+			Timestamp: timestamp,
+			Operation: Operation(operation),
+			Old:       old,
+			New:       new,
+		}
+		changes = append(changes, change)
+	}
+	sort.Sort(ByTimestamp(changes))
+
+	for _, change := range changes {
+		if change.Timestamp.Before(target) {
+			break
+		}
+
+		keychain := strings.Split(change.Keychain, ":")
+		if len(keychain) == 1 {
+			switch change.Operation {
+			case Addition:
+				delete(*data, keychain[0])
+			case Deletion:
+				raw, _ := change.Old.(string)
+
+				var record map[string]interface{}
+				json.Unmarshal([]byte(raw), &record)
+
+				(*data)[keychain[0]] = record
+			}
+		} else if len(keychain) == 2 {
+			_, isMap := (*data)[keychain[0]].(map[string]interface{})
+			if !isMap {
+				return nil, errors.New("record wasn't a map")
+			}
+
+			switch change.Operation {
+			case Addition:
+				delete((*data)[keychain[0]].(map[string]interface{}), keychain[1])
+			case Modification:
+				(*data)[keychain[0]].(map[string]interface{})[keychain[1]] = change.Old
+			case Deletion:
+				(*data)[keychain[0]].(map[string]interface{})[keychain[1]] = change.Old
+			}
+		}
+	}
+
+	if err := Write(db, "TestWrite", Archive, data, true); err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
--- tests/io_test.go
+++ tests/io_test.go
@@ -1,14 +1,17 @@
 package tests
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
+	"time"
 
 	_ "github.com/lib/pq" // postgres driver
 	"github.com/sr-revops/brickhouse"
 )
 
 func TestUpdate(t *testing.T) {
-	d := map[string]interface{}{
+	testData := map[string]interface{}{
 		"1": map[string]interface{}{
 			"a": "foo",
 			"b": "bar",
@@ -26,7 +29,73 @@ func TestUpdate(t *testing.T) {
 		},
 	}
 
-	if err := brickhouse.Update(db, "test", &d); err != nil {
+	if err := brickhouse.Update(db, "TestUpdate", &testData); err != nil {
 		t.Error(err)
+	}
+}
+
+func TestRestore(t *testing.T) {
+	targetTimes := map[string]time.Time{}
+	testSets := map[string]map[string]interface{}{
+		"base": map[string]interface{}{
+			"abc": map[string]interface{}{
+				"name":  "Joe Schmoe",
+				"email": "j.schmoe@gmail.com",
+			},
+		},
+		"cellModification": map[string]interface{}{
+			"abc": map[string]interface{}{
+				"name":  "Joe Schmoe",
+				"email": "j@protonmail.ch",
+			},
+		},
+		"cellAddition": map[string]interface{}{
+			"abc": map[string]interface{}{
+				"name":  "Joe Schmoe",
+				"email": "j@protonmail.ch",
+				"phone": "012-345-6789",
+			},
+		},
+		"cellDeletion": map[string]interface{}{
+			"abc": map[string]interface{}{
+				"name":  "Joe Schmoe",
+				"phone": "012-345-6789",
+			},
+		},
+		"recordAddition": map[string]interface{}{
+			"abc": map[string]interface{}{
+				"name":  "Joe Schmoe",
+				"phone": "012-345-6789",
+			},
+			"def": map[string]interface{}{
+				"name":  "Moe Schloe",
+				"phone": "987-654-3210",
+			},
+		},
+		"recordDeletion": map[string]interface{}{
+			"def": map[string]interface{}{
+				"name":  "Moe Schloe",
+				"phone": "987-654-3210",
+			},
+		},
+	}
+
+	for operation, data := range testSets {
+		if err := brickhouse.Update(db, "TestRestore", &data); err != nil {
+			t.Error(err)
+		}
+		targetTimes[operation] = time.Now()
+	}
+
+	for operation, targetTime := range targetTimes {
+		if outData, err := brickhouse.Restore(db, "TestRestore", targetTime); err != nil {
+			t.Error(err)
+		} else {
+			if !reflect.DeepEqual(*outData, testSets[operation]) {
+				fmt.Println(*outData)
+				fmt.Println(testSets[operation])
+				t.Errorf("restore failed on %s", operation)
+			}
+		}
 	}
 }
